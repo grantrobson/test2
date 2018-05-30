36eef07a652670e8dde9c99545d83ff1badcbc49
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-05-30 09:53:48 +0100

    DDCNLS-EC Add google sheets test and code stub

---
+++ src/main/scala/googlesheets/GoogleSheetsClient.scala
@@ -0,0 +1,6 @@
+package googlesheets
+
+object GoogleSheetsClient {
+
+}
+
---
+++ src/test/scala/googlesheets/GoogleSheetsClientTest.scala
@@ -0,0 +1,21 @@
+package googlesheets
+
+import org.scalatest.{Matchers, OptionValues, WordSpecLike}
+
+class GoogleSheetsClientTest extends WordSpecLike with Matchers with OptionValues {
+  "GoogleSheetsClient" should {
+    "do something" in {
+
+//      val gdc = GoogleSheetsClient.apply("ddcnls-errors-list", "")
+//
+//      val expectedResult =     Seq(
+//        Seq("one", "two"),
+//        Seq("three", "four")
+//      )
+//
+//      val result = gdc.
+//      result shouldBe expectedResult
+
+    }
+  }
+}
